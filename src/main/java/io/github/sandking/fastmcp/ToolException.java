1f82119eef1e63a2f8a52df5a7932eda1cee1267
Sandking <sk.jinxing@gmail.com>
2026-06-24 17:08:52 +0800

    Create ToolException.java

---
+++ src/main/java/io/github/sandking/fastmcp/ToolException.java
@@ -0,0 +1,11 @@
+package io.github.sandking.fastmcp;
+
+public class ToolException extends RuntimeException {
+    public ToolException(String message) {
+        super(message);
+    }
+
+    public ToolException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
