737fb7d29e692012931651b400ab8a0786b14bd0
Sandking <sk.jinxing@gmail.com>
2026-06-24 17:09:07 +0800

    Create ToolNotFoundException.java

---
+++ src/main/java/io/github/sandking/fastmcp/ToolNotFoundException.java
@@ -0,0 +1,7 @@
+package io.github.sandking.fastmcp;
+
+public final class ToolNotFoundException extends ToolException {
+    public ToolNotFoundException(String message) {
+        super(message);
+    }
+}
